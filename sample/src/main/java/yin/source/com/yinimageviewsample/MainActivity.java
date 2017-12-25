daeb0798d36ff7d0f6b0279804f034b9e0d8356e
yin <1035909389@qq.com>
2017-12-25 14:34:42 +0800

    添加 SquareImageView

--- sample/src/main/java/yin/source/com/yinimageviewsample/MainActivity.java
+++ sample/src/main/java/yin/source/com/yinimageviewsample/MainActivity.java
@@ -1,13 +1,41 @@
 package yin.source.com.yinimageviewsample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnCircleImage;
+    private Button btnSquareImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCircleImage = (Button) findViewById(R.id.btn_circle_image);
+        btnSquareImage = (Button) findViewById(R.id.btn_square_image);
+
+        btnCircleImage.setOnClickListener(this);
+        btnSquareImage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.btn_circle_image:
+                intent = new Intent(getApplicationContext(), CircleImageActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_square_image:
+                intent = new Intent(getApplicationContext(), SquareImageActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
---
+++ yinimageviews/src/main/java/yin/source/com/yinimageview/SquareImageView.java
@@ -0,0 +1,37 @@
+package yin.source.com.yinimageview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+/**
+ * 宽高相等的正方形 ImageView，一般与 scaleType="centerCrop" 配合使用保证内容充满控件
+ * Created by yin on 2017/12/25.
+ */
+
+public class SquareImageView extends android.support.v7.widget.AppCompatImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = getMeasuredWidth();
+        int measuredHeight = getMeasuredHeight();
+
+        if (measuredWidth != measuredHeight) {
+            int max = Math.min(measuredWidth, measuredHeight);
+            setMeasuredDimension(max, max);
+        }
+    }
+}
