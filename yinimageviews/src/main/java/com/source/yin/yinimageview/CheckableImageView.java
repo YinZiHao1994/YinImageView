fe7ef68bfaad98ab5002973f43c5bfecda2fb918
yin <1035909389@qq.com>
2018-07-10 09:18:25 +0800

    添加 CheckableImageView 类

---
+++ yinimageviews/src/main/java/com/source/yin/yinimageview/CheckableImageView.java
@@ -0,0 +1,52 @@
+package com.source.yin.yinimageview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.Checkable;
+
+public class CheckableImageView extends android.support.v7.widget.AppCompatImageView implements Checkable {
+
+    private boolean isChecked;
+    private static final int[] CHECKED_STATE_SET = {android.R.attr.state_checked};
+
+    public CheckableImageView(Context context) {
+        this(context, null);
+    }
+
+    public CheckableImageView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CheckableImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public int[] onCreateDrawableState(final int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (isChecked()) {
+            mergeDrawableStates(drawableState, CHECKED_STATE_SET);
+        }
+        return drawableState;
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        if (isChecked() != checked) {
+            isChecked = checked;
+            refreshDrawableState();
+        }
+    }
+
+    @Override
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    @Override
+    public void toggle() {
+        setChecked(!isChecked());
+    }
+
+}
